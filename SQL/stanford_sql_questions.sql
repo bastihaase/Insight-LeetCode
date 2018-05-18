b5762ad9a4d43f8a538c61e8c00f9274797f3c1c
bastihaase <bhaase2909@gmail.com>
2018-05-18 14:52:23 -0700

    added Q3 in stanford_sql_question.SQL

--- SQL/stanford_sql_questions.sql
+++ SQL/stanford_sql_questions.sql
@@ -23,3 +23,23 @@ ON h1.ID = l.ID1
 INNER JOIN Highschooler h2
 ON h2.ID = l.ID2
 AND h1.grade - h2.grade > 1;
+
+
+-- Q3
+--    For every pair of students who both like each other,
+--    return the name and grade of both students. Include each pair only once,
+--    with the two names in alphabetical order.
+
+SELECT h1.name,
+       h1.grade,
+       h2.name,
+       h2.grade
+FROM Highschooler h1
+INNER JOIN Likes l1
+ON h1.ID = l1.ID1
+INNER JOIN Likes l2
+ON l1.ID1 = l2.ID2
+AND l1.ID2 = l2.ID1
+INNER JOIN Highschooler h2
+ON h2.ID = l2.ID1
+AND h1.name < h2.name;
