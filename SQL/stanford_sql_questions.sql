b98803a1a434866f4704ce748b702d3dcf2a6633
bastihaase <bhaase2909@gmail.com>
2018-05-19 14:45:21 -0700

    added Q9 in stanford_sql_question.SQL

--- SQL/stanford_sql_questions.sql
+++ SQL/stanford_sql_questions.sql
@@ -44,7 +44,7 @@ INNER JOIN Highschooler h2
 ON h2.ID = l2.ID1
 AND h1.name <= h2.name;
 
--- Q4
+-- Q7
 -- For each student A who likes a student B where the two are not friends,
 -- find if they have a friend C in common (who can introduce them!).
 -- For all such trios, return the name and grade of A, B, and C.
@@ -71,3 +71,24 @@ AND f2.ID2 = f3.ID2
 INNER JOIN Highschooler h3
 ON h3.ID = f2.ID2
 WHERE f.ID1 IS NULL;
+
+-- Q9. Find the name and grade of all students who are liked by more than one other student.
+
+CREATE
+TEMPORARY
+TABLE likecount
+AS
+(SELECT h1.ID,
+       COUNT(*) AS likes
+FROM Highschooler h1
+INNER JOIN Likes l
+ON h1.ID = l.ID2
+GROUP BY h1.ID);
+
+
+SELECT h.name,
+       h.grade
+FROM Highschooler h
+INNER JOIN likecount c
+ON h.ID = c.ID
+AND c.likes > 1;
