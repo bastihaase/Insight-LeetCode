c3664a4f475465cf458f95518567896bb3f324bd
bastihaase <bhaase2909@gmail.com>
2018-05-20 19:48:11 -0700

    added Prim in graph.py

--- Python/graph.py
+++ Python/graph.py
@@ -61,7 +61,47 @@ class Graph():
                     pq.decreaseKey(x[0], dist)
         return entries
 
+    def prims(self, start):
+        pq = PriorityQueue()
+        entries = dict([(str(v), float('inf')) for v in self.vertices])
+        entries[start] = ()
+        pq.buildHeap([(float('inf'), v) for v in self.vertices])
+        pq.decreaseKey(start, 0)
+        visited = set()
 
+        new_vertices = []
+        new_edges = dict([(x, []) for x in self.vertices])
+        last = None
+
+        while not pq.isEmpty():
+            current = pq.delMin()
+            visited.add(current)
+            new_vertices.append(visited)
+
+            if last:
+                new_edges[last].append(current)
+                new_edges[current].append(last)
+
+            last = current[1]
+            for v in self.adj[current]:
+                dist = entries[str(current)] + v[1]
+                old = entries[str(v[0])]
+                if old > dist:
+                    entries[str(v[0])] = dist
+                    pq.decreaseKey(v[0], dist)
+
+        return Graph(new_vertices, new_edges)
+
+
+
+
+
+
+
+
+
+
+# For connected component and Dijkstra
 vertices = [1, 2, 3, 4, 5, 6, 7]
 adj_List = {1: [(2, 2), (3, 4), (4, 1)],
             2: [(1, 2), (3, 1), (5, 2)],
@@ -74,3 +114,16 @@ adj_List = {1: [(2, 2), (3, 4), (4, 1)],
 graph = Graph(vertices, adj_List)
 print(graph.connected_components())
 print(graph.Dijkstra(1))
+
+
+# For travelling Salesman: graph satisfies triangle inequality
+
+vertices2 = [1, 2, 3, 4, 5]
+
+adj_List2 = {
+    1: [(2, 2), (3, 3), (4, 1), (5, 3)],
+    2: [(1, 2), (3, 2), (4, 3), (5, 3)],
+    3: [(1, 3), (2, 2), (4, 4), (5, 2)],
+    4: [(1, 1), (2, 3), (3, 4), (5, 3)],
+    5: [(1, 2), (2, 3), (3, 2), (4, 3)]
+}
