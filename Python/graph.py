c654741c2a58b696686934466af9d3a2776c2414
bastihaase <bhaase2909@gmail.com>
2018-05-20 19:14:22 -0700

    added graph.py

---
+++ Python/graph.py
@@ -0,0 +1,76 @@
+# adj list: {vertex: [(other vertex, weight), ...]}
+from pythonds.graphs import PriorityQueue
+
+
+class Graph():
+    def __init__(self, vertices, adj_list):
+        self.vertices = vertices
+        self.adj = adj_list
+
+    def number_of_vertices(self):
+        return len(self.vertices)
+
+    def contains(self, vertex):
+        return vertex in self.vertices
+
+    def dfs(self, start, connected=set()):
+        if not self.contains(start):
+            return set()
+        connected.add(start)
+        for y in self.adj[start]:
+            if y[0] not in connected:
+                connected = connected.union(self.dfs(y[0]), connected)
+        return connected
+
+    def bfs(self, start):
+        connected = set()
+        queue = [start]
+        while not queue == []:
+            y = queue.pop(0)
+            connected.add(y)
+            for x in self.adj[y]:
+                if x[0] not in connected:
+                    queue.append(x[0])
+        return connected
+
+    def connected_components(self):
+        unvisited = set(self.vertices)
+        components = []
+        while unvisited != set():
+            x = unvisited.pop()
+            comp = self.bfs(x)
+            components.append(comp)
+            unvisited = unvisited.difference(comp)
+        return components
+
+    def Dijkstra(self, start):
+        # initialize the heap with infinity dist
+        pq = PriorityQueue()
+        entries = dict([(str(v), float('inf')) for v in self.vertices])
+        entries[str(start)] = 0
+        pq.buildHeap([(float('inf'), v) for v in self.vertices])
+        pq.decreaseKey(start, 0)
+        # do the dijkstra step
+        while not pq.isEmpty():
+            cur = pq.delMin()
+            for x in self.adj[cur]:
+                dist = entries[str(cur)] + x[1]
+                old = entries[str(x[0])]
+                if old > dist:
+                    entries[str(x[0])] = dist
+                    pq.decreaseKey(x[0], dist)
+        return entries
+
+
+vertices = [1, 2, 3, 4, 5, 6, 7]
+adj_List = {1: [(2, 2), (3, 4), (4, 1)],
+            2: [(1, 2), (3, 1), (5, 2)],
+            3: [(1, 4), (2, 1), (7, 1)],
+            4: [(1, 1), (7, 2)],
+            5: [(2, 2)],
+            6: [],
+            7: [(4, 2), (3, 1)]}
+
+graph = Graph(vertices, adj_List)
+print(graph.connected_components())
+print(graph.Dijkstra(1))
