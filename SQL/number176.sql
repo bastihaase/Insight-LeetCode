be6356211652269ec60cb95d5d94171921ae93ce
bastihaase <bhaase2909@gmail.com>
2018-05-23 14:15:58 -0700

    added #176 in SQL

--- SQL/number176.sql
+++ SQL/number176.sql
@@ -25,3 +25,8 @@ ORDER BY Salary DESC
 LIMIT 1 OFFSET 1
 )
 ) AS SecondHighestSalary;
+
+SELECT MAX(e2.Salary) AS SecondHighestSalary
+FROM Employee e1
+INNER JOIN Employee e2
+WHERE e1.Salary > e2.Salary
