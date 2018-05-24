eb01a6d20891008b5891a114cb5cf6ee306eaab5
bastihaase <bhaase2909@gmail.com>
2018-05-23 18:31:58 -0700

    added #83 in Python

---
+++ Python/number83.py
@@ -0,0 +1,38 @@
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+# Example 1:
+
+# Input: 1->1->2
+# Output: 1->2
+
+# Example 2:
+
+# Input: 1->1->2->3->3
+# Output: 1->2->3
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+
+
+        current = head
+
+        while current.next:
+            while current.next and current.val == current.next.val:
+                current.next = current.next.next
+            current = current.next
+            if not current:
+                break
+
+        return head
